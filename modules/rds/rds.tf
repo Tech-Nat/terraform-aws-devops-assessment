19128f0f995bb7eab40b283bdb87231d7e1271d3
Nat Ndlovu <natndlovu@NTHNs-MacBook-Pro.local>
2024-10-31 21:00:43 +0200

    dev deploy

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -9,16 +9,6 @@ resource "aws_db_subnet_group" "main" {
 }
 
 # Create RDS Instance
-resource "random_password" "db_password" {
-  length  = 16
-  special = true
-}
-
-variable "db_username" {
-  type    = string
-  default = "admin"  # Default username, or generate dynamically if needed
-}
-
 resource "aws_db_instance" "main" {
   allocated_storage      = var.db_allocated_storage
   storage_type           = "gp2"
@@ -26,7 +16,7 @@ resource "aws_db_instance" "main" {
   instance_class         = var.db_instance_class
   db_name                = var.db_name
   username               = var.db_username
-  password               = random_password.db_password.result
+  password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [var.security_group_id]
   skip_final_snapshot    = true
