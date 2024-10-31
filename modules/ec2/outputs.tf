896b65bd178a27509a98b79638bc25f4e754c236
Nat Ndlovu <natndlovu@NTHNs-MacBook-Pro.local>
2024-10-31 05:34:28 +0200

    dev deploy

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,4 +1,3 @@
-# output.tf
 output "ec2_public_ip" {
   value = aws_instance.nginx-webserver.public_ip  # Replace with your actual instance resource
   description = "The public IP address of the EC2 instance"
