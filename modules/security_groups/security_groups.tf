33d737372bb553fc1e30271c3656e2bdb407b280
Nat Ndlovu <natndlovu@NTHNs-MacBook-Pro.local>
2024-10-31 21:16:50 +0200

    dev deploy

--- modules/security_groups/security_groups.tf
+++ modules/security_groups/security_groups.tf
@@ -1,3 +1,4 @@
+# EC2 Security Group
 resource "aws_security_group" "ec2_sg" {
     vpc_id = var.vpc_id
 
@@ -14,4 +15,27 @@ resource "aws_security_group" "ec2_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow inbound access from EC2 on the database port"
+  vpc_id      = var.vpc_id
+
+  # Allow incoming database traffic from the EC2 security group
+  ingress {
+    from_port       = 3306 
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]  
+  }
+
+  # Optionally deny all outbound traffic from RDS
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
