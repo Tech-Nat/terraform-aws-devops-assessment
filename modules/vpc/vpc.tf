b747c8cdf6a6fcab9ff3f2c5ca08dff9fc862cbc
Nat Ndlovu <natndlovu@NTHNs-MacBook-Pro.local>
2024-11-01 06:45:09 +0200

    added variables

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -7,12 +7,15 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidr
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.private_subnet_cidr
+  availability_zone = "us-east-1b"
+  map_customer_owned_ip_on_launch = false
 }
 
 resource "aws_internet_gateway" "igw" {
